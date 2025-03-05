402c1ed834dd1ba1ac9914382324e5123f64d0b7
muki119 <mugagga.kimera04@gmail.com>
2025-03-05 12:59:42 +0000

    added shell for Security layer

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        connection testConnection = new connection("localhost",25565).listen();
+        while(true){
+            String text = new Scanner(System.in).nextLine()+'\n';
+            testConnection.sendData(text.getBytes());
+
+        }
     }
 }
---
+++ src/SecurityLayer.java
@@ -0,0 +1,13 @@
+public class SecurityLayer {
+
+    public void encrypt(){
+
+    }
+    public void decrypt(){
+
+    }
+    public void handshake(){
+
+    }
+}
+
